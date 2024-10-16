5a6234fcec010d5f47e873aee2d91c59baca3a1b
maricos0119 <maricos@outlook.jp>
2024-10-16 22:38:46 +0900

    更新

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO user(name, gender, age, email)
+    VALUES ("キラメキ太郎", "男性", 27, 'taro.kirameki@mail.com');
+INSERT INTO user(name, gender, age, email)
+    VALUES ("キラメキ次郎", "男性", 22, 'jiro.kirameki@email.com');
+INSERT INTO user(name, gender, age, email)
+    VALUES("キラメキ花子", "女性", 25, 'hanako.kirameki@email.com');
