617e9271e6355ae7270faf6be740ca6f88761762
maricos0119 <maricos@outlook.jp>
2024-10-24 21:50:36 +0900

    更新

--- src/main/java/com/techacademy/controller/UserController.java
+++ src/main/java/com/techacademy/controller/UserController.java
@@ -59,16 +59,25 @@ public class UserController {
 
     /** User更新画面を表示 */
     @GetMapping("/update/{id}/")
-    public String getUser(@PathVariable("id") Integer id, Model model) {
-        // Modelに登録
-        model.addAttribute("user", service.getUser(id));
+    public String getUser(@PathVariable(name = "id", required =false) Integer id, User user, Model model) {
+        if (id != null) {
+            // 一覧画面から遷移。サービスから取得したUserをモデルにセットModelに登録
+            model.addAttribute("user", service.getUser(id));
+        }else {
+            //postUser()から遷移。引数のuserをModelにセット
+            model.addAttribute("user",user);
+        }
         // User更新画面に遷移
         return "user/update";
     }
 
     /** User更新処理 */
     @PostMapping("/update/{id}/")
-    public String postUser(User user) {
+    public String postUser(@Validated User user, BindingResult res,Model model) {
+        if (res.hasErrors()) {
+            //エラーがある場合、getUserを呼び出し
+            return getUser(null, user, model);
+        }
         // User登録
         service.saveUser(user);
         // 一覧画面にリダイレクト
--- src/main/java/com/techacademy/entity/User.java
+++ src/main/java/com/techacademy/entity/User.java
@@ -1,7 +1,5 @@
 package com.techacademy.entity;
 
-import org.hibernate.validator.constraints.Length;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -9,12 +7,18 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne; // 追加
+import jakarta.persistence.PreRemove; // 追加
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+import org.springframework.transaction.annotation.Transactional; // 追加
+
 import lombok.Data;
 
 @Data
@@ -23,7 +27,7 @@ import lombok.Data;
 public class User {
 
     /** 性別用の列挙型 */
-    public static enum Gender{
+    public static enum Gender {
         男性, 女性
     }
 
@@ -38,7 +42,7 @@ public class User {
     @Length(max=20)
     private String name;
 
-    /** 性別。２桁。列挙型（文字列） */
+    /** 性別。2桁。列挙型（文字列） */
     @Column(length = 2)
     @Enumerated(EnumType.STRING)
     @NotNull
@@ -55,5 +59,18 @@ public class User {
     @Length(max=50)
     private String email;
 
+    // ----- 追加ここから -----
+    @OneToOne(mappedBy="user")
+    private Authentication authentication;
 
+    /** レコードが削除される前に行なう処理 */
+    @PreRemove
+    @Transactional
+    private void preRemove() {
+        // 認証エンティティからuserを切り離す
+        if (authentication!=null) {
+            authentication.setUser(null);
+        }
+    }
+    // ----- 追加ここまで -----
 }
